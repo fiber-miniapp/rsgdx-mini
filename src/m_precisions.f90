5e1c195be0dd790fc9fd048a76ea029b7b12d548
mikami3heart <mikami3heart@gmail.com>
2014-09-25 14:36:27 +0900

    delete contents

--- src/m_precisions.f90
+++
@@ -1,8 +0,0 @@
-!C****
-!C**** module : precisions
-!C****
-      MODULE     precisions
-      IMPLICIT NONE
-      integer, parameter :: KP = 4
-      integer, parameter :: DP = 8
-      END MODULE precisions
